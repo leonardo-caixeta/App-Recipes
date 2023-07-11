5a6d6faa535b801c7e963d817de4e78955f6f3ed
Mariana Correa <mpcs270391@gmail.com>
2023-07-11 18:15:34 -0300

    feat: Requisitos 57, 59, 61 - Cria estrutuda da página de perfil com botões redirecionando para páginas de done e favoritos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,26 @@
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import profileIcon from '../images/profileIcon.svg';
 
 export default function Profile() {
   return (
     <>
       <Header haveSearch={ false } title="Profile" />
+      <main>
+        <img alt="Imagem do perfil" src={ profileIcon } />
+        <p data-testid="profile-email">email</p>
+        <section>
+          <Link to="./done-recipes" data-testid="profile-done-btn">Done Recipes</Link>
+          <Link
+            to="./favorite-recipes"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </Link>
+        </section>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </main>
       <Footer />
     </>
   );
