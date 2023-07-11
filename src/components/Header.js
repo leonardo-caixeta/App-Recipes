53c24f77e47f624088d2eafb8ececa8f2e4b12eb
leonardo caixeta <leonardocaixeta@gmail.com>
2023-07-11 14:33:06 -0300

    feat: implements Header component propTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -45,3 +46,8 @@ export default function Header({ haveSearch, title }) {
     </div>
   );
 }
+
+Header.propTypes = {
+  haveSearch: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+};
