54b0288a5075e19aff78c116bd868322e26385e7
leonardo caixeta <leonardocaixeta@gmail.com>
2023-07-11 14:54:13 -0300

    feat: implements requisito 10

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <div>SearchBar</div>
+    <div>
+      <label htmlFor="ingredient-search-radio">
+        <input
+          type="radio"
+          id="ingredient-search-radio"
+          data-testid="ingredient-search-radio"
+        />
+        Busca por ingrediente
+      </label>
+      <label htmlFor="name-search-radio">
+        <input
+          type="radio"
+          id="name-search-radio"
+          data-testid="name-search-radio"
+        />
+      </label>
+      Busca por nome
+      <label htmlFor="first-letter-search-radio">
+        <input
+          type="radio"
+          id="first-letter-search-radio"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+      Busca pela primeira letra
+      <button data-testid="exec-search-btn">
+        Buscar
+      </button>
+    </div>
   );
 }
 
--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 
 export default function Drinks() {
   return (
     <div>
       <Header haveSearch title="Drinks" />
+      <SearchBar />
     </div>
   );
 }
