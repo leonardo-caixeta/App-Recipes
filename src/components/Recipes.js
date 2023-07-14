d49d628fef66d977731d4a45eebb4df9ce1632c7
leonardo caixeta <leonardocaixeta@gmail.com>
2023-07-14 14:38:24 -0300

    att (sem componente mapedRecipes)

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,7 +5,7 @@ import Categories from './Categories';
 import FoodContext from '../contexts/FoodContext';
 
 function Recipes() {
-  const { recipes } = useContext(FoodContext);
+  const { recipes, toggleRenderFiltered } = useContext(FoodContext);
   const { pathname } = window.location;
 
   const getRecipePath = (recipe) => {
@@ -20,7 +20,7 @@ function Recipes() {
       <Categories />
       <br />
       <br />
-      {recipes.map((recipe, index) => (
+      {toggleRenderFiltered === false && recipes.map((recipe, index) => (
         <Link
           to={ getRecipePath(recipe) }
           key={ index }
