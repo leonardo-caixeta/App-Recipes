9de0feb9f3f3b380e239e2b7883f1947d478db08
Mariana Correa <mpcs270391@gmail.com>
2023-07-16 16:09:18 -0300

    refactor: Muda estrutura de Header, inserindo o searchbar no componente de header

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,13 +1,11 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import SearchBar from '../components/SearchBar';
 import Recipes from '../components/Recipes';
 
 export default function Drinks() {
   return (
     <>
       <Header haveSearch title="Drinks" />
-      <SearchBar food="drinks" />
       <Recipes />
 
       <Footer />
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,15 +1,12 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Recipes from '../components/Recipes';
-import SearchBar from '../components/SearchBar';
 
 export default function Meals() {
   return (
     <>
       <Header haveSearch title="Meals" />
-      <SearchBar food="meals" />
-
-      <Recipes />
+      {/* <Recipes /> */}
       <Footer />
     </>
   );
