e1fb161c0a909993982da13c6ccd8a388d3cecab
anthony-steffen <anthony.steffen@outlook.com.br>
2023-07-19 15:19:12 -0300

    fix:src/tests/helpers/localStorageMock.js

---
+++ src/tests/helpers/localStorageMock.js
@@ -0,0 +1,18 @@
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+export default localStorageMock;
